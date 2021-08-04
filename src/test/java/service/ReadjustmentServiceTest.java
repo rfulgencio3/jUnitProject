f46e5e9235b37a3a395cd5fcdef2934735df0338
Ricardo Fulgencio Alves <ricardo.alves@socialbank.com.br>
2021-08-04 20:28:16 -0300

    Refectoring ReadjustementServiceTests using jUnit notations.

--- src/test/java/service/ReadjustmentServiceTest.java
+++ src/test/java/service/ReadjustmentServiceTest.java
@@ -1,5 +1,6 @@
 package service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tddProject.model.Employee;
 import tddProject.model.Performance;
@@ -12,33 +13,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReadjustmentServiceTest {
 
+    private ReadjustmentService readjustmentService;
+    private Employee employee;
+
+    @BeforeEach
+    public void startTestParams() {
+        this.readjustmentService = new ReadjustmentService();
+        this.employee = new Employee("Ana", LocalDate.now(), new BigDecimal("1000.00"));
+    }
+
     @Test
     public void readjustmentMustBeThreePercentWhenPerformanceEqualsToBeDesired() {
-        ReadjustmentService readjustmentService = new ReadjustmentService();
-        Employee employee = new Employee("Ana", LocalDate.now(), new BigDecimal("1000.00"));
-
         readjustmentService.grantReadjustment(employee, Performance.TO_BE_DESIRED);
-
         assertEquals(new BigDecimal("1030.00"), employee.getSalary());
     }
 
     @Test
     public void readjustmentMustBeThreePercentWhenPerformanceEqualsToGood() {
-        ReadjustmentService readjustmentService = new ReadjustmentService();
-        Employee employee = new Employee("Ana", LocalDate.now(), new BigDecimal("1000.00"));
-
         readjustmentService.grantReadjustment(employee, Performance.GOOD);
-
         assertEquals(new BigDecimal("1150.00"), employee.getSalary());
     }
 
     @Test
     public void readjustmentMustBeThreePercentWhenPerformanceEqualsToExcellent() {
-        ReadjustmentService readjustmentService = new ReadjustmentService();
-        Employee employee = new Employee("Ana", LocalDate.now(), new BigDecimal("1000.00"));
-
         readjustmentService.grantReadjustment(employee, Performance.EXCELLENT);
-
         assertEquals(new BigDecimal("1200.00"), employee.getSalary());
     }
 }
